f426a7275d4c3db81c73fe085d69acf672731789
Emma Blixt <emma.blixt13@gmail.com>
2018-03-22 16:20:12 +0100

    Add images
    Add images that are fetched from an api

---
+++ show-users-from-api/EpicPicture.php
@@ -0,0 +1,86 @@
+<?php
+defined('ABSPATH') or die('No script kiddies please!');
+
+class EpicPicture
+{
+  private $album_id;
+  private $id;
+  private $title;
+  private $url;
+  private $thumbnail_url;
+
+  function __construct()
+  {
+    return $this->init();
+  }
+
+  /**
+  * Initialize the class & add WP hooks
+  */
+  public function init()
+  {
+    add_shortcode('display-pictures', array($this, 'show_pictures'));
+  }
+
+  /**
+  * Fetch data from the api
+  * @return object $images that contains all the user information
+  */
+  private function fetch_image_data()
+  {
+    try {
+    $response = wp_remote_get( 'https://jsonplaceholder.typicode.com/photos' );
+    $images = json_decode( $response['body'] );
+
+    } catch ( Exception $ex ) {
+        $images = null;
+    }
+    return $images;
+  }
+
+  /**
+  * Set data from the api
+  * @param object $image
+  */
+  private function set_image_values($image)
+  {
+    $this->id            = $image->id;
+    $this->album_id      = $image->albumId;
+    $this->title         = $image->title;
+    $this->url           = $image->url;
+    $this->thumbnail_url = $image->thumbnailUrl;
+  }
+
+  /**
+  * Set data from the api
+  * @return array $output that contains the image data
+  */
+  public function get_image_data()
+  {
+    if (null == ($json_response = $this->fetch_image_data())) :
+      $output = "<h1>Sorry, I couldn't find the api</h1>";
+    else :
+      foreach ($json_response as $image) :
+        $this->set_image_values($image);
+        $output[] = array(
+            'id'            =>  $image->id,
+            'album_id'      =>  $image->albumId,
+            'title'         =>  $image->title,
+            'url'           =>  $image->url,
+            'thumbnail_url' =>  $image->thumbnailUrl,
+        );
+      endforeach;
+    endif;
+    return $output;
+  }
+
+  /**
+  * Fetch template to display pictures
+  */
+  public function show_pictures()
+  {
+    display_template('image-without-gallery.php');
+  }
+}
+
+$show_pictures = new EpicPicture();
